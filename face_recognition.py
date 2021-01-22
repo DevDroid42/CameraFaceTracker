78520564f85b8e0f64b5d3933eeb4fe792905504
DevDroid42 <19912037+DevDroid42@users.noreply.github.com>
2021-01-22 19:33:48 +0000

    video face tracking works!

--- face_recognition.py
+++ face_recognition.py
@@ -1,41 +1,54 @@
 import io
 import picamera
-import cv2
 import numpy
+from picamera.array import PiRGBArray # Generates a 3D RGB array
+from picamera import PiCamera # Provides a Python interface for the RPi Camera Module
+import time # Provides time-related functions
+import cv2 # OpenCV library
+ 
+# Initialize the camera
+camera = PiCamera()
+ 
+# Set the camera resolution
+camera.resolution = (320, 240)
+ 
+# Set the number of frames per second
+camera.framerate = 32
+ 
+# Generates a 3D RGB array and stores it in rawCapture
+raw_capture = PiRGBArray(camera, size=(320, 240))
+ 
+face_cascade = cv2.CascadeClassifier('/home/pi/Face/haarcascade_frontalface_default.xml')
+# Wait a certain number of seconds to allow the camera time to warmup
+time.sleep(0.1)
+ 
+# Capture frames continuously from the camera
+for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
+     
+    # Grab the raw NumPy array representing the image
+    image = frame.array
 
-
-
-for imgNum in range(5):
-    #Create a memory stream so photos doesn't need to be saved in a file
-    stream = io.BytesIO()
-    #Get the picture (low resolution, so it should be quite fast)
-    #Here you can also specify other parameters (e.g.:rotate the image)
-    with picamera.PiCamera() as camera:
-        camera.resolution = (320, 240)
-        camera.capture(stream, format='jpeg')
-
-    #Convert the picture into a numpy array
-    buff = numpy.frombuffer(stream.getvalue(), dtype=numpy.uint8)
-    
-    #Now creates an OpenCV image
-    image = cv2.imdecode(buff, 1)
-    
-    #https://github.com/opencv/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
-    #Load a cascade file for detecting faces
-    face_cascade = cv2.CascadeClassifier('/home/pi/Face/haarcascade_frontalface_default.xml')
-    
     #Convert to grayscale
     gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-    
+
     #Look for faces in the image using the loaded cascade file
     faces = face_cascade.detectMultiScale(gray, 1.1, 5)
-    
-    print ("Found {}" + str(len(faces)) + " face(s)")
-    
+
     #Draw a rectangle around every found face
     for (x,y,w,h) in faces:
         cv2.rectangle(image,(x,y),(x+w,y+h),(255,255,0),4)
         print ("Position X:" + str(x) + " Y:" + str(y) + " W:" + str(w) + " h:" + str(h))
-
-    #Save the result image
-    cv2.imwrite("result"+str(imgNum)+".jpg",image)
+    # Display the frame using OpenCV
+    #cv2.imshow("Frame", image)
+     
+    # Wait for keyPress for 1 millisecond
+    key = cv2.waitKey(1) & 0xFF
+     
+    # Clear the stream in preparation for the next frame
+    raw_capture.truncate(0)
+     
+    # If the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        cv2.rectangle(image,(x,y),(x+w,y+h),(255,255,0),4)
+        cv2.imwrite("result.jpg",image)
+        break
